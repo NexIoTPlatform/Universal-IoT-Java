b2ce681314ef2326ddc8fc88272a318c3b984f0a
hex <liulh@wasu.com>
2025-08-31 19:21:05 +0800

    fix:增加部分注视，删除无用代码

--- cn-universal-web/src/main/java/cn/universal/web/config/GlobalExceptionHandler.java
+++ cn-universal-web/src/main/java/cn/universal/web/config/GlobalExceptionHandler.java
@@ -16,7 +16,6 @@ import cn.hutool.core.exceptions.ExceptionUtil;
 import cn.hutool.json.JSONUtil;
 import cn.universal.common.domain.R;
 import cn.universal.common.exception.IoTException;
-import cn.universal.common.utils.DingTalkUtil;
 import cn.universal.common.utils.ErrorUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -52,7 +51,9 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  /** 未知异常处理 */
+  /**
+   * 未知异常处理
+   */
   @ExceptionHandler(value = Exception.class)
   @ResponseBody
   public R exceptionHandler(Exception e) {
@@ -136,11 +137,12 @@ public class GlobalExceptionHandler {
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public R handleMysql(Exception e) {
     log.error("数据库异常，请及时处理={}", ErrorUtil.errorInfoToString(e));
-    DingTalkUtil.send("数据库异常，请及时处理" + ExceptionUtil.getSimpleMessage(e));
     return R.error("数据库异常，请联系管理员");
   }
 
-  /** 处理认证异常 */
+  /**
+   * 处理认证异常
+   */
   @ExceptionHandler(AuthenticationException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public void handleAuthenticationException(
@@ -163,7 +165,9 @@ public class GlobalExceptionHandler {
     response.getWriter().write(JSONUtil.toJsonStr(errorResponse));
   }
 
-  /** 处理 OAuth2 认证异常 */
+  /**
+   * 处理 OAuth2 认证异常
+   */
   @ExceptionHandler(OAuth2AuthenticationException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public void handleOAuth2AuthenticationException(
@@ -193,7 +197,9 @@ public class GlobalExceptionHandler {
     response.getWriter().write(JSONUtil.toJsonStr(errorResponse));
   }
 
-  /** 获取错误信息 */
+  /**
+   * 获取错误信息
+   */
   private String getErrorMessage(AuthenticationException exception) {
     if (exception instanceof BadCredentialsException) {
       return exception.getMessage();
--- cn-universal-web/src/main/java/cn/universal/web/controller/VersionController.java
+++ cn-universal-web/src/main/java/cn/universal/web/controller/VersionController.java
@@ -12,32 +12,22 @@
 
 package cn.universal.web.controller;
 
-import cn.hutool.json.JSONObject;
-import cn.hutool.json.JSONUtil;
-import cn.universal.admin.platform.service.BatchFunctionTask;
-import cn.universal.common.monitor.NetworkMonitorSingleton;
 import cn.universal.persistence.consistent.DeviceShardingRouter;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.logging.LogLevel;
 import org.springframework.boot.logging.logback.LogbackLoggingSystem;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/** 程序版本 */
+/**
+ * 程序版本
+ */
 @RestController
 @RequestMapping("/test")
 @Slf4j
@@ -45,94 +35,27 @@ public class VersionController {
 
   @GetMapping("/v1/debug/log")
   public Object update(@RequestParam String name, @RequestParam LogLevel level) {
-    log.info("修改日志级别 name={} ,level={}", name, level.name());
+    log.info("动态修改日志级别 name={} ,level={}", name, level.name());
     if (LogLevel.INFO.equals(level) || LogLevel.DEBUG.equals(level)) {
       loggingSystem.setLogLevel(name, level);
-      return "ok";
+      return "success";
     }
     return "只允许debug和info";
   }
 
-  /** TCL门锁推送接口校验 */
-  @GetMapping("/check")
-  public Object tclCheck() {
-    return "success";
-  }
 
-  @GetMapping("/serverCheck")
-  public Object serverCheck() {
-    Map<String, Boolean> connectionStatus = NetworkMonitorSingleton.getConnectionStatus();
-    HashMap<String, Boolean> hashMap = new HashMap<>();
-    for (Map.Entry<String, Boolean> q : connectionStatus.entrySet()) {
-      hashMap.put(q.getKey(), q.getValue());
-      if ("tcp://mqtt.iotuniv.cn:1883".equals(q.getKey())) {
-        hashMap.put("EMQ", q.getValue());
-      } else if ("tcp://218.108.247.88:11883".equals(q.getKey())) {
-        hashMap.put("chuanmeiMqtt", q.getValue());
-      } else if ("125.210.48.169:9876".equals(q.getKey())) {
-        hashMap.put("rocketMq", q.getValue());
-      } else if ("msgpush.ctaiot.cn:16651".equals(q.getKey())) {
-        hashMap.put("ctwingMqtt", q.getValue());
-      } else if ("tcp://218.108.146.92:11883".equals(q.getKey())) {
-        hashMap.put("lvzhouMqtt", q.getValue());
-      }
-    }
-    return hashMap;
-  }
+  @Resource
+  private LogbackLoggingSystem loggingSystem;
 
-  @GetMapping("/topicConsumer")
-  public Object serverCheck(String topic) {
-    //    return rocketMQMonitor.queryDefaultTopicExistConsumer(topic);
-    return "success";
-  }
+  @Resource
+  private DeviceShardingRouter tableShardStrategyByIotId;
 
-  @GetMapping("/version")
-  public Object versionInformation() {
-    return readGitProperties();
-  }
-
-  @Resource private LogbackLoggingSystem loggingSystem;
-  @Resource private StringRedisTemplate stringRedisTemplate;
-  @Resource private DeviceShardingRouter tableShardStrategyByIotId;
-
-  //  @GetMapping("/debug/tcp/devices")
-  //  public Object tcpDevices(@RequestParam String name, @RequestParam LogLevel level) {
-  //    log.info("查看设备连接状态");
-  //    Map<TcpDeviceCtx, ChannelHandlerContext> tcpMap = TcpDeviceStorage.getInstance()
-  //        .nettyChannelList();
-  //    return tcpMap;
-  //  }
 
   @GetMapping("/log/shard")
   public Object getTableId(@RequestParam String iotId) {
     return getTableShardIdByIotId(iotId);
   }
 
-  /** 读取文件 */
-  private JSONObject readGitProperties() {
-    FileSystemResource classPathResource = new FileSystemResource("./version.json");
-    InputStream inputStream = null;
-    try {
-      inputStream = classPathResource.getInputStream();
-    } catch (IOException e) {
-      log.error("获取文件异常", e);
-    }
-    return JSONUtil.parseObj(readFromInputStream(inputStream));
-  }
-
-  /** 读取文件里面的值 */
-  private String readFromInputStream(InputStream inputStream) {
-    StringBuilder stringBuilder = new StringBuilder();
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-      String line;
-      while ((line = br.readLine()) != null) {
-        stringBuilder.append(line);
-      }
-    } catch (IOException e) {
-      log.error("读取文件失败", e);
-    }
-    return stringBuilder.toString();
-  }
 
   private String getTableShardIdByIotId(String iotId) {
     String tableId = tableShardStrategyByIotId.generateTableName("表号为", iotId);
@@ -140,19 +63,9 @@ public class VersionController {
     return s[1];
   }
 
-  @Resource private BatchFunctionTask batchFunctionTask;
-
-  @GetMapping("/log/shaw")
-  public void getBatchFunctionTask() {
-    batchFunctionTask.doTask();
-  }
-
-  @GetMapping("/log/shared")
-  public void getBatchFunctionTas2k() {
-    batchFunctionTask.consumer();
-  }
-
-  /** echo - 打印请求体和请求头 */
+  /**
+   * echo - 打印请求体和请求头
+   */
   @RequestMapping("/echo")
   public Object testLog(@RequestBody String body, HttpServletRequest request) {
 
