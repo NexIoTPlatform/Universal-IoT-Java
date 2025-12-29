902bed1830792d778c972cdcfab6f7ebfebab5eb
Join <123456@gmail.com>
2025-12-29 09:02:42 +0800

    refactor(wvp): wvp有安全风险，暂不开源

--- cn-universal-protocol/cn-wvp-protocol/src/main/java/cn/wvp/protocol/handler/WvpCommandHandler.java
+++
@@ -1,9 +0,0 @@
-package cn.wvp.protocol.handler;
-
-import cn.universal.common.domain.R;
-import cn.wvp.protocol.entity.WvpDownRequest;
-
-public interface WvpCommandHandler {
-  String getSupportedCommand();
-  R<?> handle(WvpDownRequest request);
-}
